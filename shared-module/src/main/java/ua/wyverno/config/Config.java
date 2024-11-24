c96db81b40c110772150459f6e44b903fe1d4de6
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2024-11-24 18:24:53 +0200

    Update Config Autocreating file properties

--- shared-module/src/main/java/ua/wyverno/config/Config.java
+++ shared-module/src/main/java/ua/wyverno/config/Config.java
@@ -6,9 +6,12 @@ public class Config {
     protected Config() {}
 
     protected void setToken(String token) {
+        if (token.isEmpty()) throw new IllegalArgumentException("Token cannot be empty!!!");
         this.token = token;
     }
     protected void setProjectID(String projectID) {
+        if (projectID.isEmpty()) throw new IllegalArgumentException("ProjectID field cannot be empty!!!");
+        if (!projectID.matches("\\d+")) throw new IllegalArgumentException("ProjectID must be numeric type!!!");
         this.projectID = Long.parseLong(projectID);
     }
     public String getToken() {
--- shared-module/src/main/java/ua/wyverno/config/ConfigLoader.java
+++ shared-module/src/main/java/ua/wyverno/config/ConfigLoader.java
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.nio.file.Files;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
@@ -21,27 +22,25 @@ public class ConfigLoader {
     public ConfigLoader() throws IOException, ConfigDamagedException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         File file = new File("config.properties");
         Properties properties = new Properties();
+        List<Field> fieldsConfig = this.getConfigFields();
+
+        if (!file.exists() || file.isDirectory()) { // Якщо не існує, або це директорія, тоді створюємо конфіг файл
+            this.createProperties(properties, file, fieldsConfig.stream().map(Field::getName).toList());
+            throw new FileNotFoundException(file.toPath().toAbsolutePath() + " - Config file not exists!!!");
+        }
         properties.load(Files.newInputStream(file.toPath()));
 
         // Перевіряємо на наявність полів
-        Field[] fields = Config.class.getDeclaredFields();
-        List<String> missingFieldConfig = new ArrayList<>();
-        for (Field field : fields) {
-            field.setAccessible(true);
-            if (!properties.containsKey(field.getName())) {
-                missingFieldConfig.add(field.getName());
-            }
-        }
-        // Якщо є поля які не присутні у конфігу додаємо їх та повідомляємо про помилку
-        if (missingFieldConfig.size() > 0) {
-            missingFieldConfig.forEach(fieldName -> properties.put(fieldName, ""));
-            properties.store(Files.newOutputStream(file.toPath()), null);
-            throw new ConfigDamagedException(file + " No has all fields!");
+        List<String> missingFields = this.getMissingFields(properties, fieldsConfig);
+        if (!missingFields.isEmpty()) { // Якщо не всі присутні поля
+            this.createProperties(properties, file, missingFields);
+            throw new ConfigDamagedException(file + " no has all field Config!");
         }
 
         this.config = new Config();
 
-        for (Field field : fields) { // Доступ повторно не потрібно ставити, бо вже було раніше надано.
+        // Додаємо всі значення з конфігу до Конфіг классу
+        for (Field field : fieldsConfig) { // Доступ повторно не потрібно ставити, бо вже було раніше надано.
             String fieldName = field.getName();
             String s1 = fieldName.substring(0, 1).toUpperCase();
             String other = fieldName.substring(1);
@@ -50,6 +49,22 @@ public class ConfigLoader {
             setMethod.setAccessible(true);
             setMethod.invoke(this.config, properties.getProperty(fieldName));
         }
+    }
+
+    private List<Field> getConfigFields() {
+        return Arrays.stream(Config.class.getDeclaredFields())
+                .peek(field -> field.setAccessible(true))
+                .toList();
+    }
+    private List<String> getMissingFields(Properties properties, List<Field> fields) {
+        return fields.stream()
+                .map(Field::getName)
+                .filter(fieldName -> !properties.containsKey(fieldName))
+                .toList();
+    }
+    private void createProperties(Properties properties, File file, List<String> fields) throws ConfigDamagedException, IOException {
+        fields.forEach(fieldName -> properties.put(fieldName, ""));
+        properties.store(Files.newOutputStream(file.toPath()), null);
     }
 
     public Config getConfig() {
