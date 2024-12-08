8a797c84bafd5bbf8f24f1c8e5e6a5dfa76e8da9
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2024-12-09 01:58:17 +0200

    Created class for Import Translation

---
+++ core-module/src/main/java/ua/wyverno/console/commands/ImportTranslationCommand.java
@@ -0,0 +1,25 @@
+package ua.wyverno.console.commands;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ua.wyverno.console.Command;
+import ua.wyverno.console.ConsoleCommand;
+import ua.wyverno.sync.translation.ImportTranslationService;
+
+@ConsoleCommand(command = "/import-translation", description = "Importing translation from sheets to Crowdin")
+@Component
+public class ImportTranslationCommand implements Command {
+
+    private final ImportTranslationService importTranslationService;
+
+    @Autowired
+    public ImportTranslationCommand(ImportTranslationService importTranslationService) {
+        this.importTranslationService = importTranslationService;
+    }
+
+    @Override
+    public void execute(String[] args) {
+        System.out.println("Starting importing translation.");
+        this.importTranslationService.importTranslationToCrowdin();
+    }
+}
---
+++ core-module/src/main/java/ua/wyverno/sync/translation/ImportTranslationService.java
@@ -0,0 +1,11 @@
+package ua.wyverno.sync.translation;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImportTranslationService {
+
+    public void importTranslationToCrowdin() {
+
+    }
+}
