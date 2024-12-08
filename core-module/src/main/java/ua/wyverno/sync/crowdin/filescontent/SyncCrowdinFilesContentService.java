93480b4d9a63e40fc0dace5f33f1e138c1d5efca
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2024-12-09 00:36:10 +0200

    Empty Component for new strategy synchronization

---
+++ core-module/src/main/java/ua/wyverno/sync/crowdin/filescontent/SyncCrowdinFilesContentService.java
@@ -0,0 +1,15 @@
+package ua.wyverno.sync.crowdin.filescontent;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import ua.wyverno.sync.crowdin.sourcestrings.SyncCrowdinSourceStringsService;
+
+@Service
+public class SyncCrowdinFilesContentService {
+    private final static Logger logger = LoggerFactory.getLogger(SyncCrowdinFilesContentService.class);
+
+    public void synchronizationToFilesContent() {
+
+    }
+}
